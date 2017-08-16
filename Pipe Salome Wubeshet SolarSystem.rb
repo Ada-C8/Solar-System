ef38b40c852a3fe75b5019f3d21551c772cfc5e1
Salome Wubeshet <swubeshet@gmail.com>
2017-08-16 09:13:25 -0700

    Update Pipe Salome Wubeshet SolarSystem.rb

--- Pipe Salome Wubeshet SolarSystem.rb
+++ Pipe Salome Wubeshet SolarSystem.rb
@@ -7,15 +7,11 @@ class SolarSystem
 		return @all_planets
 	end
 	
-	def planets
-		return @all_planets	
-	end
+	# def planets
+	# 	return @all_planets	
+	# end
 
-	def add
-		puts "What planet do you want to add?\n"
-		new_planet = gets.chomp
-		@planets << new_planet
-	end
+end
 
 class Planet
 	attr_accessor :name, :color, :surface
@@ -24,23 +20,45 @@ class Planet
 	@name = name
 	@color = color
 	@surface = surface
+	@all_planets = {}
 	end
 
 	def return_att
-		puts "Planet Name: #{name}\nPlanet Color: #{color}\nPlanet Surface: #{surface}"
+	count = 1
+	puts "\n#{count} Planet Name: #{name}\n#{count+1} Planet Color: #{color}\n#{count+2} Planet Surface: #{surface}"
+	
+	end
+
+	def store (key)
+		@all_planets = {key => [@name, @color, @surface]}
+	end
+
+	def add
+	puts "What planet do you want to add?\n"
+	new_planet = gets.chomp
+		@all_planets << new_planet
 	end
 	
-	def planets
-	return @all_planets
-	end
+	# def planets
+	# return @all_planets
+	# end
 end
 
-earth = SolarSystem.new("Earth", "Blue", "Waters and Rocks")
-mars = SolarSystem.new("Mars", "Red", "Rocky")
-venus = SolarSystem.new("Venus", "Green", "Granitic Rock")
+earth = Planet.new("Earth", "Blue", "Waters and Rocks")
+mars = Planet.new("Mars", "Red", "Rocky")
+venus = Planet.new("Venus", "Green", "Granitic Rock")
 
-all_planets = [earth, mars, venus].add
+all_planets = [earth, mars, venus]
+
+puts earth.store("earth")
+puts mars.store("mars")
+puts venus.store("venus")
 
 puts earth.return_att
 puts mars.return_att
 puts venus.return_att
+
+# puts mars.return_att
+# puts venus.return_att
+
+
